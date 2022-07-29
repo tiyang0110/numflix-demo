fe21826efec6c063a18840d1a0f83a267fa1d542
tiyang0110 <tiyang0110@douzone.com>
2022-07-29 17:41:10 +0900

    Numflix 기본구조 세팅

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  red: '#E51013',
+  black: {
+    veryDark: '#141414',
+    darker: '#1881818',
+    lighter: '#2F2F2F',
+  },
+  white: {
+    lighter: '#FFF',
+    darker: '#e5e5e5',
+  }
+}
