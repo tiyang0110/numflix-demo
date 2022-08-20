f57f53cefd3f5f1b66fd48bcdec087baa649f0b5
tiyang0110 <peble_@naver.com>
2022-08-20 20:06:58 +0900

    tv할차례

--- src/Routes/TV.tsx
+++ src/Routes/TV.tsx
@@ -1,5 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import styled from "styled-components";
+
+
+const Wrapper = styled.div`
+  background-color: black;
+  overflow: hidden;
+`;
+
 function TV(){
-  return null;
+  const {data, isLoading} = useQuery(['tv', 'arirangToday'], ()=>{});
+
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
 }
 
 export default TV;
--- src/api.ts
+++ src/api.ts
@@ -45,8 +45,56 @@ export interface IMovieSearchData {
   results: IMovie[];
 }
 
+export interface ILatestMovie {
+    adult: boolean;
+    backdrop_path: string|null;
+    belongs_to_collection: string|null;
+    budget: number;
+    genres: [],
+    homepage: string;
+    id: number;
+    imdb_id: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string|null;
+    production_companies: [];
+    production_countries: [];
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    spoken_languages: [
+        {
+            english_name: string;
+            iso_639_1: string;
+            name: string;
+        }
+    ],
+    status: string;
+    tagline: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+
+// MOVIE API
 export function getMovies(){
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then((response) => response.json());
+}
+
+export function getLatestMovies(){
+  return fetch(`${BASE_PATH}/movie/latest?api_key=${API_KEY}&language=en-US`).then((response) => response.json());
+}
+
+export function getTopRatedMovies(){
+  return fetch(`${BASE_PATH}/movie/top_rated?api_key=${API_KEY}&page=1`).then((response) => response.json());
+}
+
+export function getUpcomingMovies(){
+  return fetch(`${BASE_PATH}/movie/upcoming?api_key=${API_KEY}&page=1`).then((response) => response.json());
 }
 
 export function getMovieDetail(movieId:string){
@@ -55,6 +103,13 @@ export function getMovieDetail(movieId:string){
 
 export function searchMovie(keyword:string|null){
   return fetch(`${BASE_PATH}/search/movie?api_key=${API_KEY}&query=${keyword}`).then((response) => response.json());
+}
+
+
+// TV API
+
+export function getArirangTodayTVs(){
+
 }
 
 export function serachTV(keyword:string|null){
